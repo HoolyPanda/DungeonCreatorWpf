3d78262a203c9a4a3012714c13925caa2307d2cb
Pavlo <pavlov.ss10@gmail.com>
2018-07-17 21:36:14 +0300

    --
    Descktop app to make dungeon creating process easier
    also trying to use WPF instead of WinForms

---
+++ DungeonCreatorWpf/Dungeon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCreatorWpf
+{
+    class Dungeon
+    {
+        public Dungeon()
+        {
+
+        }
+        struct Data
+        {
+            Encounter[] encounters;
+            string answer;
+            string description;
+            string toenter;
+            byte[] image;
+        }
+    }
+}
---
+++ DungeonCreatorWpf/Encounter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCreatorWpf
+{
+    class Encounter
+    {
+        string name;
+        string dis;
+        Action[] Actions=new Action[4];
+    }
+}
